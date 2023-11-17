f1e05a6c6902d69fb31a761b84ff9d54c6b1f4b1
Fabricio da Silva Lisboa <150758493+fablisboa2006@users.noreply.github.com>
2023-11-16 22:23:47 -0300

    hora de codar codigos

---
+++ fundamentos.js
@@ -0,0 +1,20 @@
+//Fundamentos
+//var>declarar varável nome>nome da variavel valor> prórpia variavel
+var minhaVariavel = "Olá Mundo";
+
+console.log(minhaVariavel);
+
+// variaveis e tipos de dados
+
+var meunumero = 10;
+console.log(meunumero);
+
+console.log(meunumero + 5);
+
+ console.log("10" + 5); //entre aspas o número vira texto mais ainda sim ele junta os números e não soma
+
+ console.log(typeof meunumero);
+
+ var booleano = true; // ou false > verdadeiro ou falso
+
+ console.log(typeof booleano);
